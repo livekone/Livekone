c623961ce2e60cb9d1c3399fd454b510b96b47fc
nekohasy <63384336+nekohasy@users.noreply.github.com>
2025-08-03 14:11:03 +0900

    お問い合わせフォームをGoogleフォームに変更

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,5 +1,4 @@
 import styles from './page.module.css';
-import ContactForm from '@/app/_components/ContactForm';
 
 export default function Page() {
   return (
@@ -9,7 +8,16 @@ export default function Page() {
         <br />
         内容確認後、担当者より通常3営業日以内にご連絡いたします。
       </p>
-      <ContactForm />
+      <iframe
+        src="https://docs.google.com/forms/d/e/1FAIpQLSerjm-5Uu2YYxj5f_nCf4Hlua4lj6g78SzEh-cmrcd0OqIbbQ/viewform?embedded=true"
+        width="100%"
+        height="1200"
+        frameborder="0"
+        marginheight="0"
+        marginwidth="0"
+      >
+        読み込んでいます…
+      </iframe>
     </div>
   );
 }
