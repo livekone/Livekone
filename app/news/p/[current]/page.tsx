5a482cd2b1487d46e5b98e0072beef6bc4e55e3e
nekohasy <63384336+nekohasy@users.noreply.github.com>
2025-08-03 18:52:50 +0900

    軽微な修正

--- app/news/p/[current]/page.tsx
+++ app/news/p/[current]/page.tsx
@@ -4,23 +4,24 @@ import Pagination from '@/app/_components/Pagination';
 import ArticleList from '@/app/_components/NewsList';
 
 type Props = {
-  params: {
-    current: string;
-  };
+  params: Promise<{ current: string }>;
 };
 
 export const revalidate = 60;
 
 export default async function Page({ params }: Props) {
-  const current = parseInt(params.current as string, 10);
+  const { current } = await params;
+  const currentPage = parseInt(current, 10);
+
   const data = await getNewsList({
     limit: NEWS_LIST_LIMIT,
-    offset: NEWS_LIST_LIMIT * (current - 1),
+    offset: NEWS_LIST_LIMIT * (currentPage - 1),
   });
+
   return (
     <>
       <ArticleList articles={data.contents} />
-      <Pagination totalCount={data.totalCount} current={current} basePath="/news" />
+      <Pagination totalCount={data.totalCount} current={currentPage} basePath="/news" />
     </>
   );
 }
